82716ad3ea6204552a46cea17096c9a010ef5089
student <ok>
2022-10-28 12:08:08 -0400

    yeah

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,22 +2,34 @@ import java.lang.reflect.Method;
 
 public class Calculator {
     private String opps = "*/+-%^";
+    private String str;
 
     public Calculator() {
 
+
     }
 
-    public void eval(int a, int b, String x) {
+    public double eval(double a, double b, String x) {
         int y = opps.indexOf(x);
-        System.out.println(y);
         switch (y) {
             case 0:
                 return a*b;
+            case 1:
+                return a/b;
+            case 2:
+                return a+b;
+            case 3:
+                return a-b;
+            case 4:
+                return a%b;
+            case 5:
+                return Math.pow(a, b);
+            default:
                 break;
         }
 
 
-
+        return 0.0;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -13,24 +13,27 @@ public class Main {
 
         String opps = "*/+-%^";
         int l=-1;
+        int i;
 
 
 
-
-        for (int i = 0; i<exp.length(); i+=1) {
+        for (i=0; i<exp.length(); i+=1) {
             l = opps.indexOf(exp.substring(i, i+1));
             if (l!=-1) {
                 break;
             }
         }
 
-        int n1 = Integer.parseInt(exp.substring(0, l-1));
-        int n2 =  Integer.parseInt(exp.substring(l));
-        String o = exp.substring(l-1, l);
+        double n1 = Double.parseDouble(exp.substring(0, i).replaceAll("\\s", ""));
+        double n2 = Double.parseDouble(exp.substring(i+1).replaceAll("\\s", ""));
+
+
+        String o = opps.substring(l, l+1);
+
 
         Calculator calc = new Calculator();
 
-        calc.eval(n1, n2, o);
+        System.out.println(calc.eval(n1, n2, o));
 
 
 
